a5619477be8ac0f10fa44f188aad6e02898004be
Cody Knapp <KrafDinner@gmail.com>
2016-06-26 13:48:48 -0400

    Refactoring service initialization to setup method

--- Babysitter/test/com/wcknapp/pillar/ValidationServiceTest.java
+++ Babysitter/test/com/wcknapp/pillar/ValidationServiceTest.java
@@ -2,38 +2,39 @@ package com.wcknapp.pillar;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ValidationServiceTest {
 	ValidationService uut;
 	
+	@Before
+	public void setUp() {
+		uut = new ValidationService();
+	}
+	
 	@Test
 	public void ensureValidateTimeReturnsFalseWhenSimpleNumberIsPassed() {
-		uut = new ValidationService();
 		assertFalse(uut.validateTime("5"));
 	}
 
 	@Test
 	public void ensureValidateTimeReturnsFalseWhenTimeIsMissingAMPM() {
-		uut = new ValidationService();
 		assertFalse(uut.validateTime("5:00"));
 	}
 	
 	@Test
 	public void ensureValidateTimeReturnsFalseWhenInvalidAMTimeIsPassed() {
-		uut = new ValidationService();
 		assertFalse(uut.validateTime("13:00AM"));
 	}
 	
 	@Test
 	public void ensureValidateTimeReturnsTrueWhenValidPMTimeIsPassed() {
-		uut = new ValidationService();
 		assertTrue(uut.validateTime("5:00PM"));
 	}
 	
 	@Test
 	public void ensureValidateTimeReturnsTrueWhenValidAMTimeIsPassed() {
-		uut = new ValidationService();
 		assertTrue(uut.validateTime("3:00AM"));
 	}
 }
